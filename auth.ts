a477a9b9f55a37e8df71d6dba9b30109889218b2
vpittamp <vinod@pittampalli.com>
2024-07-29 12:42:40 +0000

    azure auth working

--- auth.ts
+++ auth.ts
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import AzureAd from "next-auth/providers/azure-ad"
 
 import type { NextAuthConfig, Session } from 'next-auth';
 
@@ -24,6 +25,11 @@ export const config = {
         },
       },
     }),
+    AzureAd({
+      clientId: process.env.AUTH_MICROSOFT_ENTRA_ID_ID,
+      clientSecret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET,
+      tenantId: process.env.AUTH_MICROSOFT_ENTRA_ID_TENANT_ID,
+    })
   ],
   basePath: '/api/auth',
   callbacks: {
@@ -45,16 +51,26 @@ export const config = {
       } else {
         console.log('Access token expired getting new one');
         try {
-          const response = await fetch('https://oauth2.googleapis.com/token', {
+          const response = await fetch('https://login.microsoftonline.com/0c4da9c5-40ea-4e7d-9c7a-e7308d4f8e38/oauth2/v2.0/token', {
             headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
             body: new URLSearchParams({
-              client_id: process.env.AUTH_GOOGLE_ID as string, // Type assertion
-              client_secret: process.env.AUTH_GOOGLE_SECRET as string, // Type assertion
+              client_id: process.env.AUTH_MICROSOFT_ENTRA_ID_ID as string, // Type assertion
+              client_secret: process.env.AUTH_MICROSOFT_ENTRA_ID_SECRET as string, // Type assertion
               grant_type: 'refresh_token',
               refresh_token: token.refresh_token as string, // Type assertion
             }),
             method: 'POST',
           });
+          // const response = await fetch('https://oauth2.googleapis.com/token', {
+          //   headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+          //   body: new URLSearchParams({
+          //     client_id: process.env.AUTH_GOOGLE_ID as string, // Type assertion
+          //     client_secret: process.env.AUTH_GOOGLE_SECRET as string, // Type assertion
+          //     grant_type: 'refresh_token',
+          //     refresh_token: token.refresh_token as string, // Type assertion
+          //   }),
+          //   method: 'POST',
+          // });
 
           const tokens = await response.json();
 
